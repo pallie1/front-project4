6ff77e8aa5dfd597afec9a3bd28f8592e517212e
Allie Pacyga <alliep@Allies-MacBook-Air.local>
2020-08-12 18:31:21 -0400

    added some styling to the feed

--- front-end-bean/src/Components/Feed/Feed.jsx
+++ front-end-bean/src/Components/Feed/Feed.jsx
@@ -1,42 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import apiUrl from '../../apiConfig';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import apiUrl from "../../apiConfig";
+import axios from "axios";
+import "./Feed.scss";
 
 const Feed = () => {
+  const [reviews, setReviews] = useState([]);
 
-    const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    const allRevAPICall = async () => {
+      try {
+        const res = await axios(`${apiUrl}/reviews`);
+        setReviews(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
-    useEffect(() => {
-        const allRevAPICall = async () =>{
-            try {
-                const res = await axios(`${apiUrl}/reviews`);
-                setReviews(res.data);
-            } catch (err) {
-                console.error(err);
-            }
-        }
+    allRevAPICall();
+  }, []);
 
-        allRevAPICall();
-    },[])
+  console.log("reviews - ", reviews);
 
-    console.log('reviews - ', reviews)
+  const mappedRevs = reviews.map((review) => {
+    console.log("user inside review single- ", review.user.username);
 
-    const mappedRevs = reviews.map( (review) => {
-        return (
-        <div key={review.id} >
-            <img src={review.img} alt='user submited coffee shop picture' />
-            <p>{review.rate}</p>
-            <p>{review.content}</p>
+    if (review.user.length !== null) {
+      return (
+        <div className={'review-div'} key={review.id}>
+          <p>{review.user.username}</p>
+          <img src={review.img} alt="user submited coffee shop picture" />
+          <p>{review.rate}</p>
+          <p>{review.content}</p>
         </div>
-        )
-    })
+      );
+    } else { return <h1>Loading...</h1>}
+  });
 
-    return (
-        <>
-        <h1>Feed</h1>
-        <div>{mappedRevs}</div>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Where have other users have been.</h1>
+      <div>{mappedRevs}</div>
+    </>
+  );
+};
 
 export default Feed;
